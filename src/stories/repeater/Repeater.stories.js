920b34282ba5808057252a246fe957e08345441c
Thamizhanban <thamizh@swipepages.com>
2024-02-22 02:26:20 +0530

    bugfix - position wihile dragging the repeater is fixed.

--- src/stories/repeater/Repeater.stories.js
+++ src/stories/repeater/Repeater.stories.js
@@ -14,18 +14,27 @@ export default {
     },
     args: {
         value: ["Item 1", "Item 2", "Item 3"]
-    }
+    },
+    decorators: [
+        (Story) => (
+            <div style={{ width:"350px", margin: "0 auto" }}>
+                {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+                <Story />
+            </div>
+        ),
+    ]
 };
 
-export const Input = ({ value }) => {
+export const Input = ({ value, ...args }) => {
     const [tempValue, setTempValue] = useState(value)
-    return <Repeater value={tempValue} onChange={(newOptions) => setTempValue(newOptions)} />
+    return <Repeater {...args} value={tempValue} onChange={(newOptions) => setTempValue(newOptions)} />
 }
-export const Custom = ({ value }) => {
+export const Custom = ({ value, ...args }) => {
     const defaultTempValue = [{ name: "Item 1", link: "link 1" }, { name: "Item 2", link: "link 2" }]
     const [tempValue, setTempValue] = useState(defaultTempValue);
     return (
         <Repeater
+            {...args}
             value={tempValue}
             onChange={(newOptions) => setTempValue(newOptions)}
             customItem={(option, index) => (
